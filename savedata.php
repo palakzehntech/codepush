439a9266194f24e228d2f32e0507c72e9e5b56da
palakzehntech <palak.zehntech@gmail.com>
2023-08-24 19:02:03 +0530

    crud index added

---
+++ savedata.php
@@ -0,0 +1,15 @@
+<?php  
+echo $stu_name = $_POST['sname'];
+echo $stu_address = $_POST['saddress'];
+echo $stu_class = $_POST['class'];
+echo $stu_phone = $_POST['sphone'];
+
+    $conn=mysqli_connect("localhost","root","","crud") or die("connection failed");
+   $sql= "insert into student(sname,saddress,sclass,sphone) values ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}') ";
+   $result=mysqli_query($conn,$sql)or die("query unsuccessful");
+   header("location: http://localhost/crud/index.php");
+   mysqli_close($conn);
+   
+   
+
+?>
